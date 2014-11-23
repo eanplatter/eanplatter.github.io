c3ac24d96cb13f827c0b7fc53a0c29a2d36b816b
eanplatter <eanplatter@gmail.com>
2014-11-22 20:43:06 -0700

    time color

--- scripts.js
+++ scripts.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
       backgroundColor: '#8CCDA4'
     }, 10000, function() {
         $(".pulse").animate({
-            backgroundColor: '#D41E26'
+            backgroundColor: '#D41E26',
+            color: "#8CCDA4"
         }, 10000, function(){
             $(".pulse").animate({
                 backgroundColor: '#94CCC9'
